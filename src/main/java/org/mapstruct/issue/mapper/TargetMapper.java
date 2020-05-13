d9620fd73649e767aa34669819caf8d887b4c36f
Bidorff David <david.bidorff@orange.com>
2020-05-13 16:08:02 +0200

    Expected behavior

--- src/main/java/org/mapstruct/issue/mapper/TargetMapper.java
+++ src/main/java/org/mapstruct/issue/mapper/TargetMapper.java
@@ -1,20 +1,26 @@
 package org.mapstruct.issue.mapper;
 
+import org.mapstruct.BeanMapping;
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.issue.source.SourceA;
 import org.mapstruct.issue.source.SourceB;
 import org.mapstruct.issue.target.ImmutableTargetA;
 import org.mapstruct.issue.target.ImmutableTargetB;
+import org.mapstruct.issue.target.TargetA;
+import org.mapstruct.issue.target.TargetB;
 
 @Mapper
 public interface TargetMapper {
 
+  @BeanMapping(resultType = ImmutableTargetA.class)
   @Mapping(source = "name", target = "userName")
-  ImmutableTargetA convert(SourceA source);
+  TargetA convert(SourceA source);
 
-  @Mapping(source = "name", target = "userName")
+  @BeanMapping(resultType = ImmutableTargetB.class)
+  @InheritConfiguration
   @Mapping(source = "address", target = "userAddress")
-  ImmutableTargetB convert(SourceB source);
+  TargetB convert(SourceB source);
 
 }
